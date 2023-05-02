7f2351d28320cfd506b4ec1c841d62e31999ff57
FengchunFan <ffan005@ucr.edu>
2023-05-02 14:31:58 -0700

    add code into backup.py

--- backup.py
+++ backup.py
@@ -121,6 +121,8 @@ def algo_1(puzzle):
             maximum_nodes = q.qsize()
         temp_node = q.get() #get the first node off priority queue
         temp_puzzle = temp_node.puzzle #get the puzzle from the node at starting of priority queue
+        print("The best state to expand with g(n) =", temp_node.cost)
+        print_puzzle(temp_puzzle)
         #print_puzzle(temp_puzzle)
         if(temp_puzzle == goal): #check if is goal
             print("Goal state was reached")
